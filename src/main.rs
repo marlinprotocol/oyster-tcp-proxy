a392b7817cf98fe5511bc5b10f67a9375f267831
cryptopath <cryptopath@example.com>
2022-08-30 13:12:47 +0530

    proxy code installed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(rust_2018_idioms)]
+
+use tokio::io;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpStream};
+
+use tokio_vsock::VsockStream;
+
+use futures::FutureExt;
+use std::env;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let listen_addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.3:321".to_string());
+    let server_addr = (16, 8001);
+
+    println!("Listening on: {}", listen_addr);
+    println!("Proxying to: {:?}", server_addr);
+
+    let listener = TcpListener::bind(listen_addr).await?;
+
+    while let Ok((inbound, _)) = listener.accept().await {
+        let transfer = transfer(inbound, server_addr.clone()).map(|r| {
+            if let Err(e) = r {
+                println!("Failed to transfer; error={}", e);
+            }
+        });
+
+        tokio::spawn(transfer);
+    }
+
+    Ok(())
+}
+
+async fn transfer(mut inbound: TcpStream, proxy_addr: (u32, u32)) -> Result<(), Box<dyn Error>> {
+    let mut outbound = VsockStream::connect(proxy_addr.0, proxy_addr.1).await?;
+
+    let (mut ri, mut wi) = inbound.split();
+    let (mut ro, mut wo) = io::split(outbound);
+
+    let client_to_server = async {
+        io::copy(&mut ri, &mut wo).await?;
+        wo.shutdown().await
+    };
+
+    let server_to_client = async {
+        io::copy(&mut ro, &mut wi).await?;
+        wi.shutdown().await
+    };
+
+    tokio::try_join!(client_to_server, server_to_client)?;
+
+    Ok(())
 }
